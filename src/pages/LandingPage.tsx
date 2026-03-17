ce8745709e9ff8ce65ab1a27ebf071da4a8dc00a
Rekia Mansour Ouahchia <rmansourouahchia@gmail.com>
2026-03-17 15:47:46 +0100

    install taillwind

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,7 @@
 function LandingPage() {
   return (
     <div>
-      <h1>Welcome to the Landing Page</h1>
+      <h1 className="text-3xl font-bold bg-amber-300">Welcome to the Landing Page</h1>
     </div>
   );
 }
